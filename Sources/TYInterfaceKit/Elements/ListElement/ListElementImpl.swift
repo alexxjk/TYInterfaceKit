da719d920298b3e61f3f79cb091dfc12fbff4c1e
alexxjk <alexxjk@live.com>
2020-04-26 01:28:59 +0300

    fix

--- Sources/TYInterfaceKit/Elements/ListElement/ListElementImpl.swift
+++ Sources/TYInterfaceKit/Elements/ListElement/ListElementImpl.swift
@@ -212,8 +212,8 @@ open class ListElementImpl<TItem: Equatable, TItemElement: ListItemElementImpl<T
                 self.collectionView.performBatchUpdates({ [weak self] in
                     self?.collectionView.reloadSections(IndexSet(integer: 0))
                 }) { _ in
-                    if let lastOffset = lastOffset {
-                        collectionView.contentOffset.y = lastOffset
+                    if let lastOffset = self?.lastOffset {
+                        self?.collectionView.contentOffset.y = lastOffset
                     }
                 }
             #else
