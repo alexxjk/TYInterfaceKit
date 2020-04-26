9a80a320439cb88099cf14580301b3529b10d9e0
alexxjk <alexxjk@live.com>
2020-04-26 12:50:05 +0300

    fix

--- Sources/TYInterfaceKit/Elements/ListElement/ListElementImpl.swift
+++ Sources/TYInterfaceKit/Elements/ListElement/ListElementImpl.swift
@@ -204,14 +204,13 @@ open class ListElementImpl<TItem: Equatable, TItemElement: ListItemElementImpl<T
             reloadWithAnimation(deletedIndexPaths: deletedItems, insertedIndexPaths: insertedItems)
         } else {
             #if targetEnvironment(macCatalyst)
-                collectionView.reloadSections(IndexSet(integer: 0))
-//                self.collectionView.performBatchUpdates({ [weak self] in
-//                    self?.collectionView.reloadSections(IndexSet(integer: 0))
-//                }) { [weak self] _ in
-//                    if let lastOffset = self?.lastOffset {
-//                        self?.collectionView.contentOffset.y = lastOffset
-//                    }
-//                }
+                self.collectionView.performBatchUpdates({ [weak self] in
+                    self?.collectionView.reloadSections(IndexSet(integer: 0))
+                }) { [weak self] _ in
+                    if let lastOffset = self?.lastOffset {
+                        self?.collectionView.contentOffset.y = lastOffset
+                    }
+                }
             #else
                 self.collectionView.performUsingPresentationValues { [weak self] in
                     self?.collectionView.reloadSections(IndexSet(integer: 0))
