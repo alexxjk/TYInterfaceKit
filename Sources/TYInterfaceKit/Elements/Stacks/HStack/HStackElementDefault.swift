70e3676410a26568ba4332ee6c6cd5fbead37c5d
Alexander Martirosov <alexxjk@live.com>
2020-12-06 18:44:14 +0300

    fix

--- Sources/TYInterfaceKit/Elements/Stacks/HStack/HStackElementDefault.swift
+++ Sources/TYInterfaceKit/Elements/Stacks/HStack/HStackElementDefault.swift
@@ -42,7 +42,7 @@ public class HStackElementDefault: ElementView, HStackElement {
     
     public required init(configurator: ElementViewConfigurator) {
         super.init(configurator: configurator)
-        stack.distribution = .equalSpacing
+        stack.distribution = .fill
         stack.alignment = .top
         stack.spacing = CGFloat(spacing)
     }
