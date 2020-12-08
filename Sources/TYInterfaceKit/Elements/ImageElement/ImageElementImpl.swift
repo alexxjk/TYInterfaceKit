410c2a01ee0a273f0db29898fadd0b8b38915af8
Alexander Martirosov <alexxjk@live.com>
2020-12-08 23:20:58 +0300

    fix

--- Sources/TYInterfaceKit/Elements/ImageElement/ImageElementImpl.swift
+++ Sources/TYInterfaceKit/Elements/ImageElement/ImageElementImpl.swift
@@ -63,7 +63,7 @@ open class ImageElementImpl: ElementView, ImageElement {
         func setupImageView() {
             imageView.translatesAutoresizingMaskIntoConstraints = false
             imageView.setContentHuggingPriority(.defaultLow, for: .vertical)
-            imageView.setContentHuggingPriority(.defaultLow, for: .horizontal)
+            imageView.setContentHuggingPriority(.defaultHigh, for: .horizontal)
             imageView.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
             addSubview(imageView)
             let bc = imageView.bottomAnchor.constraint(equalTo: bottomAnchor)
